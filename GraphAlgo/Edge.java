bedbf7ce2b2d7d57b3ef9b71cde9ce06cc603a8c
akash08ce063 <akashkpatel1991@gmail.com>
2014-08-22 22:56:34 -0400

    Create Edge.java

---
+++ GraphAlgo/Edge.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication62;
+
+/**
+ *
+ * @author Ricardo KAKA
+ */
+public class Edge {
+    Vertex target;
+    double wieght;
+    Edge(Vertex target, double wieght){
+        this.target = target;
+        this.wieght  = wieght;
+    }
+    
+}
